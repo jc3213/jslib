7099e4479c64f6426e9f13cea4fc8cc57ec4c2cd
jc3213 <jc3213@users.noreply.github.com>
2022-09-20 09:03:05 +0800

    update aria2.js

--- src/aria2.js
+++ src/aria2.js
@@ -1,29 +1,38 @@
-class JSLib_Aria2 {
-    constructor (jsonrpc, secret) {
-        if (jsonrpc.startsWith('http')) {
-            var sender = this.fetch;
+class Aria2 {
+    constructor (url, token) {
+        if (url.startsWith('http')) {
+            this.call = this.fetch;
         }
-        else if (jsonrpc.starsWith('ws')) {
-            sender = this.websocket;
+        else if (url.startsWith('ws')) {
+            this.call = this.websocket;
         }
         else {
-            throw new Error('Invalid JSON RPC URI: Protocal not supported!');
+            return new Error('Invalid JSON RPC URI: protocal not supported!');
+        }
+        this.jsonrpc = url;
+        if (token) {
+            this.params = ['token:' + token];
+        }
+        else {
+            this.params = [];
         }
         this.message = function (method, options) {
-            var params = [];
-            if (secret) {
-                params.push('token:' + secret);
-            }
-            if (options) {
-                params.push(...options);
-            }
-            var message = JSON.stringify({id: '', jsonrpc: '2.0', method, params});
-            return sender(jsonrpc, message);
+            var message = this.json(method, options);
+            return this.call(message);
         };
     }
-    fetch (jsonrpc, body) {
-        return fetch(jsonrpc, {method: 'POST', body}).then(function (response) {
-            return response.json();
+    json (method, options) {
+        var params = [...this.params];
+        if (options) {
+            params.push(...options);
+        }
+        var json = {id: '', jsonrpc: '2.0', method, params};
+        return JSON.stringify(json);
+    }
+    fetch (body) {
+        return fetch(this.jsonrpc, {method: 'POST', body})
+          .then(function (response) {
+            return response.json()
         }).then(function (json) {
             var {result, error} = json;
             if (result) {
@@ -34,9 +43,9 @@ class JSLib_Aria2 {
             }
         });
     }
-    websocket (jsonrpc, message) {
+    websocket (message) {
         return new Promise(function (resolve, reject) {
-            var socket = new WebSocket(jsonrpc);
+            var socket = new WebSocket(this.jsonrpc);
             socket.onopen = function (event) {
                 socket.send(message);
             };
@@ -52,5 +61,11 @@ class JSLib_Aria2 {
                 }
             };
         });
+    }
+    get methods () {
+        return ['aria2.addUri', 'aria2.addTorrent', 'aria2.addMetalink', 'aria2.remove', 'aria2.forceRemove', 'aria2.pause', 'aria2.pauseAll', 'aria2.forcePause', 'aria2.forcePauseAll', 'aria2.unpause', 'aria2.unpauseAll', 'aria2.tellStatus', 'aria2.getUris', 'aria2.getFiles', 'aria2.getPeers', 'aria2.getServers', 'aria2.tellActive', 'aria2.tellWaiting', 'aria2.tellStopped', 'aria2.changePosition', 'aria2.changeUri', 'aria2.getOption', 'aria2.changeOption', 'aria2.getGlobalOption', 'aria2.changeGlobalOption', 'aria2.getGlobalStat', 'aria2.purgeDownloadResult', 'aria2.removeDownloadResult', 'aria2.getVersion', 'aria2.getSessionInfo', 'aria2.shutdown', 'aria2.forceShutdown', 'aria2.saveSession', 'system.multicall', 'system.listMethods', 'system.listNotifications'];
+    }
+    get notifications () {
+        return ['aria2.onDownloadStart', 'aria2.onDownloadPause', 'aria2.onDownloadStop', 'aria2.onDownloadComplete', 'aria2.onDownloadError', 'aria2.onBtDownloadComplete'];
     }
 }
