5d46b3fee85cfdc6c1d7ffc365623e20cb33844e
jc3213 <jc3213@users.noreply.github.com>
2022-09-23 15:35:54 +0800

    Update notify.js

--- ui/notify.js
+++ ui/notify.js
@@ -1,19 +1,19 @@
 class Notify {
     constructor () {
         this.overlay = document.createElement('div');
-        this.overlay.className = 'jsui_notify_overlay';
+        this.overlay.className = 'jsui-notify-overlay';
         this.css = document.createElement('style');
         this.css.type = 'text/css';
         this.css.innerText = `
-        .jsui_notify_overlay {position: fixed; top: 20px; left: 0px; z-index: 99999999;}
-        .jsui_notify_popup {position: relative; background-color: #fff; cursor: pointer; padding: 5px 10px; margin: 5px; width: fit-content; border-radius: 3px; border: 1px outset #000;}`;
+        .jsui-notify-overlay {position: fixed; top: 20px; left: 0px; z-index: 99999999;}
+        .jsui-notify-popup {position: relative; background-color: #fff; cursor: pointer; padding: 5px 10px; margin: 5px; width: fit-content; border-radius: 3px; border: 1px outset #000;}`;
         document.body.prepend(this.overlay);
         document.head.appendChild(this.css);
     }
     popup (object) {
         var {message, onclick, timeout = 10000} = object;
         var popup = document.createElement('div');
-        popup.className = 'jsui_notify_popup';
+        popup.className = 'jsui-notify-popup';
         popup.innerText = message;
         popup.addEventListener('click', function (event) {
             popup.remove();
