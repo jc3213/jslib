7799f28f4b2f7b92460bb99160be6ddebc49542c
jc3213 <jc3213@users.noreply.github.com>
2022-09-23 15:30:35 +0800

    Update notify.js

--- ui/notify.js
+++ ui/notify.js
@@ -2,12 +2,10 @@ class Notify {
     constructor () {
         this.overlay = document.createElement('div');
         this.overlay.className = 'jsui_notify_overlay';
-        this.position = 'fixed';
-        this.top = '20px';
         this.css = document.createElement('style');
         this.css.type = 'text/css';
         this.css.innerText = `
-        .jsui_notify_overlay {left: 0px; z-index: 99999999;}
+        .jsui_notify_overlay {position: fixed; top: 20px; left: 0px; z-index: 99999999;}
         .jsui_notify_popup {position: relative; background-color: #fff; cursor: pointer; padding: 5px 10px; margin: 5px; width: fit-content; border-radius: 3px; border: 1px outset #000;}`;
         document.body.prepend(this.overlay);
         document.head.appendChild(this.css);
@@ -29,11 +27,5 @@ class Notify {
         this.overlay.appendChild(popup);
         popup.style.left = ((document.documentElement.clientWidth - popup.offsetWidth) / 2) + 'px';
         return popup;
-    }
-    set top (top) {
-        this.overlay.style.top = top;
-    }
-    set position (position) {
-        this.overlay.style.position = position;
     }
 }
