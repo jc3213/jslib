4ca2ca9bf09b3d8738b4bed912665cea43984567
jc3213 <jc3213@users.noreply.github.com>
2023-05-16 18:00:04 +0800

    Update jsui.js

--- ui/jsui.js
+++ ui/jsui.js
@@ -1,55 +1,99 @@
 class JSUI {
-    new (tag, params) {
-        if (typeof tag === 'string') {
-            var node = document.createElement(tag);
-            if (typeof params !== 'object') {
-                return node;
+    constructor () {
+        var stylesheet = `@media (prefers-color-scheme: light) {
+            .jsui-menu-item, .jsui-table, .jsui-table-cell {border-color: #ffffff;}
+        }
+        @media (prefers-color-scheme: dark) {
+            .jsui-menu-item, .jsui-table, .jsui-table-cell {border-color: #000000;}
+        }
+        .jsui-menu-item {text-align: center; margin: 1px; padding: 3px; border-width: 1px; border-classes: outset;}
+        .jsui-menu-item:not(.jsui-menu-disabled):hover, .jsui-table-button:hover {cursor: pointer; filter: contrast(75%);}
+        .jsui-menu-item:not(.jsui-menu-disabled):active, .jsui-table-button:active {filter: contrast(45%);}
+        .jsui-menu-item:active, .jsui-menu-checked {border-classes: inset;}
+        .jsui-menu-disabled {padding: 4px; border-width: 0px; filter: contrast(25%);}
+        .jsui-basic-menu, .jsui-drop-menu {margin: 0px; padding: 0px; user-select: none;}
+        .jsui-basic-menu {display: flex; gap: 1px;}
+        .jsui-basic-menu > * {flex: 1;}
+        .jsui-table-head > * {background-color: #000000; color: #ffffff;}
+        .jsui-table {border-width: 1px; border-classes: solid;}
+        .jsui-table-column {display: flex; gap: 1px; margin: 1px;}
+        .jsui-table-cell, .jsui-table-button {flex: 1; padding: 5px; text-align: center; line-height: 100%; border-width: 1px; border-classes: solid;}
+        .jsui-notify-overlay {position: fixed; top: 20px; left: 0px; z-index: 99999999;}
+        .jsui-notify-popup {position: relative; background-color: #fff; cursor: pointer; padding: 5px 10px; margin: 5px; width: fit-content; border-radius: 3px; border: 1px outset #cccccc;}`;
+        var css = document.getElementById('jsui-stylesheet') ?? this.new('style').attr('id', 'jsui-stylesheet');
+        css.innerText = stylesheet;
+        css.add = function (string) {
+            css.innerText += string;
+            return css;
+        };
+        css.remove = function (string) {
+            css.innerText.replace(string, '');
+            return css;
+        };
+        document.head.append(css);
+        this.css = css;
+    }
+    new (tag) {
+        var node = document.createElement(tag ?? 'div');
+        node.text = function (string) {
+            node.innerText = string;
+            return node;
+        };
+        node.html = function (string) {
+            node.innerHTML = string;
+            return node;
+        };
+        node.empty = function () {
+            node.innerHTML = '';
+            return node;
+        };
+        node.attr = function (name, value) {
+            if (typeof name === 'object') {
+                Object.keys(name).forEach(key => {
+                    node.setAttribute(key, name[key]);
+                });
             }
-        }
-        else if (typeof tag === 'object') {
-            node = document.createElement('div');
-            params = tag;
-        }
-        else {
-            throw new Error('Invalid parameters for HTML element');
-        }
-        var {id, html, text, classes, attrs, onclick, change, timeout} = params;
-        if (id !== undefined) {
-            node.id = id;
-        }
-        if (html !== undefined) {
-            node.innerHTML = html;
-        }
-        else if (text !== undefined) {
-            node.innerText = text;
-        }
-        if (typeof classes === 'string') {
-            node.className = classes;
-        }
-        else if (Array.isArray(classes)) {
-            node.classList.add(...classes);
-        }
-        if (Array.isArray(attrs)) {
-            attrs.forEach(function (attr) {
-                var {name, value} = attr;
+            else {
                 node.setAttribute(name, value);
+            }
+            return node;
+        };
+        node.class = function (string) {
+            node.classList.toggle(string);
+            return node;
+        };
+        node.hide = function () {
+            node.style.display = 'none';
+            return node;
+        };
+        node.show = function () {
+            node.style.display = '';
+            return node;
+        };
+        node.switch = function () {
+            if (node.style.display === 'none') {
+                node.style.display = '';
+            }
+            else {
+                node.style.display = 'none';
+            }
+            return node;
+        }
+        node.onclick = function (callback) {
+            node.addEventListener('click', callback);
+            return node;
+        };
+        node.onchange = function (callback) {
+            node.addEventListener('change', callback);
+            return node;
+        };
+        node.delay = function (number) {
+            return new Promise(function (resolve) {
+                setTimeout(function () {
+                    resolve(node);
+                }, number);
             });
-        }
-        else if (typeof attrs === 'object') {
-            var {name, value} = attrs;
-            node.setAttribute(name, value);
-        }
-        if (typeof onclick === 'function') {
-            node.addEventListener('click', onclick);
-        }
-        if (typeof onchange === 'function') {
-            node.addEventListener('change', onchange);
-        }
-        if (!isNaN(timeout)) {
-            node.timeout = setTimeout(function () {
-                node.remove();
-            }, timeout);
-        }
+        };
         return node;
     }
 }
