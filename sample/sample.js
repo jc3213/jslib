205ab49c0bd122ce52490e2e7c7cc0674e1b7997
jc3213 <jc3213@users.noreply.github.com>
2023-05-16 23:18:41 +0800

    Update

--- sample/sample.js
+++ sample/sample.js
@@ -1,8 +1,18 @@
-var jsUI = new JSUIex();
+var jsUI = new JSUI();
 var filereader = new PromiseFileReader();
 var url_components = new URLComponents();
 var url_result = document.querySelector('#urlcomponents > .result');
 var url_template = document.querySelector('.template > div');
+
+var js_overlay = jsUI.new().class('jsui-notify-overlay');
+document.body.append(js_overlay);
+function notification(message) {
+    var {clientWidth} = document.documentElement;
+    var popup = jsUI.new().class('jsui-notify-popup').text(message).onclick(event => popup.remove());
+    js_overlay.append(popup);
+    popup.css('left', (clientWidth - popup.offsetWidth) / 2 + 'px');
+    return popup;
+}
 
 jsUI.css.add(`body {margin: auto; width: 600px;}
 body > div:not(.jsui-notify-overlay) {margin: 10px auto; border: 1px outset #000;}
@@ -15,7 +25,7 @@ body > div:not(.jsui-notify-overlay) {margin: 10px auto; border: 1px outset #000
 textarea {width: 100%; resize: none;}
 .jsui-manager {border: 1px solid #000;}
 .jsui-table {height: 400px;}
-.jsui-table-button {background-color: #23ade5; color: #fff;}`);
+.jsui-menu-item, .jsui-menu-cell {background-color: #23ade5; color: #fff;}`);
 
 document.querySelector('#filereader').addEventListener('change', async function (event) {
     var file = event.target.files[0];
@@ -43,42 +53,28 @@ function getURLComponnents(components) {
 }
 
 function errorURLFormat(error) {
-    var warn = document.createElement('div');
-    warn.innerText = error.message;
-    url_result.appendChild(warn);
+    var warn = jsUI.new().text(error.message);
+    url_result.append(warn);
 }
 
-var menu = jsUI.menulist([
-    {text: '按钮1', onclick: clickBtnA, attrs: getAttributes('btn1', '中文')},
-    {text: 'button2', onclick: clickBtnB, attrs: getAttributes('btn2', 'English')},
-    {text: 'ボタン3', onclick: clickBtnC, attrs: getAttributes('btn3', '日本語')}
-], false);
-
-function getAttributes(id, title) {
-    return [
-        {name: 'id', value: id},
-        {name: 'title', value: title}
-    ];
-}
+var menu = jsUI.new().class('jsui-basic-menu');
+var chi = jsUI.new().class('jsui-menu-item').text('按钮1').attr({id: 'btn1', title: '中文'}).onclick(clickBtnA);
+var eng = jsUI.new().class('jsui-menu-item').text('button2').attr({id: 'btn2', title: 'English'}).onclick(clickBtnB);
+var jpn = jsUI.new().class('jsui-menu-item').text('ボタン3').attr({id: 'btn3', title: '日本語'}).onclick(clickBtnC);
+menu.append(chi, eng, jpn);
 
 function clickBtnA(event) {
-    jsUI.notification({text: '按钮1', timeout: 3000});
+    notification('按钮1').delay(3000).then(popup => popup.remove());
     entry.value = '按钮1';
 }
 
 function clickBtnB(event) {
-    jsUI.notification({text: 'Button2', timeout: 3000});
+    notification('Button2').delay(3000).then(popup => popup.remove());
     entry.value = 'Button2';
 }
 
 function clickBtnC(event) {
-    jsUI.notification({
-        text: 'ボタン3',
-        onclick: function() {
-            alert('ボタン3を押しました！');
-        },
-        timeout: 3000
-    });
+    notification('ボタン3').onclick(event => alert('ボタン3を押しました！')).delay(3000).then(popup => popup.remove());
     entry.value = 'ボタン3';
 }
 
@@ -92,6 +88,7 @@ manager.className = 'jsui-manager';
 manager.append(menu, entry);
 document.body.appendChild(manager);
 
+/** unfixed
 jsUI.dragndrop(entry, [dropzone, manager]);
 dropzone.addEventListener('drop', event => {
     entry.value = 'Drag\'n\'Drop Complete\n已成功完成拖拽\nドラッグアンドドロップ成功';
@@ -100,16 +97,27 @@ manager.addEventListener('drop', function (event) {
     menu.after(entry);
     entry.value = 'Restored Position\n回到原来的位置\n元の位置に戻りました';
 });
+**/
 
-var jsTable = jsUI.table(['测试标题', 'Test Title', 'テストタイトル']);
-jsTable.parent = manager;
-jsTable.add([
-    {text: '中文1', onclick: clickTabCelA},
-    'English1', 
-    {text: '日本語1'}
-]);
-jsTable.add(['中文2', 'English2', '日本語2', 'Español2']);
+var table = jsUI.new().class('jsui-table').html('<div class="jsui-table-title"><div>测试标题</div><div>Test Title</div><div>テストタイトル</div></div>');
+manager.append(table);
+
+var colA = jsUI.new().class('jsui-table-column');
+var caa = jsUI.new().text('中文1').class('jsui-menu-cell').onclick(clickTabCelA);
+var cab = jsUI.new().text('English1');
+var cac = jsUI.new().text('日本語1');
+colA.append(caa, cab, cac);
+
+var colB = jsUI.new().class('jsui-table-column');
+var cba = jsUI.new().text('中文2');
+var cbb = jsUI.new().text('English2');
+var cbc = jsUI.new().text('日本語2');
+var cbd = jsUI.new().text('Español2');
+colB.append(cba, cbb, cbc, cbd);
+
+table.append(colA, colB);
+
 function clickTabCelA(event) {
-    jsUI.notification({text:'测试1\nTest 1\nテスト1'});
+    notification('测试1\nTest 1\nテスト1');
     entry.value = '测试1\nTest 1\nテスト1';
 }
