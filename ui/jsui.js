aa501e0a7942209aa96866c78683e886f126505e
jc3213 <jc3213@users.noreply.github.com>
2023-05-16 20:36:28 +0800

    Update jsui.js

--- ui/jsui.js
+++ ui/jsui.js
@@ -1,23 +1,18 @@
 class JSUI {
     constructor () {
-        var stylesheet = `@media (prefers-color-scheme: light) {
-            .jsui-menu-item, .jsui-table, .jsui-basic-cell {border-color: #ffffff;}
-        }
-        @media (prefers-color-scheme: dark) {
-            .jsui-menu-item, .jsui-table, .jsui-basic-cell {border-color: #000000;}
-        }
-        .jsui-menu-item {text-align: center; margin: 1px; padding: 3px; border-width: 1px; border-classes: outset;}
-        .jsui-menu-item:not(.jsui-menu-disabled):hover, .jsui-click-cell:hover {cursor: pointer; filter: contrast(75%);}
-        .jsui-menu-item:not(.jsui-menu-disabled):active, .jsui-click-cell:active {filter: contrast(45%);}
+        var stylesheet = `
+        .jsui-menu-item {text-align: center; margin: 1px; padding: 3px 5px; border-width: 1px; border-classes: outset;}
+        .jsui-menu-item:not(.jsui-menu-disabled):hover, .jsui-menu-cell:hover {cursor: pointer; filter: contrast(75%);}
+        .jsui-menu-item:not(.jsui-menu-disabled):active, .jsui-menu-cell:active {filter: contrast(45%);}
         .jsui-menu-item:active, .jsui-menu-checked {border-classes: inset;}
-        .jsui-menu-disabled {padding: 4px; border-width: 0px; filter: contrast(25%);}
+        .jsui-menu-disabled {padding: 4px 6px; border-width: 0px; filter: contrast(25%);}
         .jsui-basic-menu, .jsui-drop-menu {margin: 0px; padding: 0px; user-select: none;}
         .jsui-basic-menu {display: flex; gap: 1px;}
         .jsui-basic-menu > * {flex: 1;}
         .jsui-table {border-width: 1px; border-classes: solid;}
-        .jsui-table-column {display: flex; gap: 1px; margin: 1px;}
-        .jsui-table-head > * {background-color: #000000; color: #ffffff;}
-        .jsui-basic-cell, .jsui-click-cell {flex: 1; padding: 5px; text-align: center; line-height: 100%; border-width: 1px; border-classes: solid;}
+        .jsui-table > * {display: flex; gap: 1px; margin: 1px;}
+        .jsui-table > * > * {flex: 1; padding: 3px 5px; text-align: center; line-height: 100%; border-width: 1px; border-classes: solid;}
+        .jsui-table-title > * {background-color: #000000; color: #ffffff;}
         .jsui-notify-overlay {position: fixed; top: 20px; left: 0px; z-index: 99999999;}
         .jsui-notify-popup {position: relative; background-color: #fff; cursor: pointer; padding: 5px 10px; margin: 5px; width: fit-content; border-radius: 3px; border: 1px outset #cccccc;}`;
         var css = document.getElementById('jsui-stylesheet') ?? this.new('style').attr('id', 'jsui-stylesheet');
