941dc1cbb0c7c5155dde06257ff921727405606d
jc3213 <jc3213@users.noreply.github.com>
2023-05-19 10:38:20 +0800

    Update

--- ui/jsui.max.js
+++ ui/jsui.max.js
@@ -107,17 +107,17 @@ class JSUI {
     }
     stylesheet () {
         var css = this.new('style').attr('id', 'jsui-stylesheet').parent(document.head).text(`
-        .jsui-menu-item {text-align: center; margin: 1px; padding: 3px 5px; border-width: 1px; border-classes: outset;}
+        .jsui-menu-item {text-align: center; margin: 1px; padding: 3px 5px; border-width: 1px; border-style: outset;}
         .jsui-menu-item:not(.jsui-menu-disabled):hover, .jsui-menu-cell:hover {cursor: pointer; filter: contrast(75%);}
         .jsui-menu-item:not(.jsui-menu-disabled):active, .jsui-menu-cell:active {filter: contrast(45%);}
-        .jsui-menu-item:active, .jsui-menu-checked {border-classes: inset;}
+        .jsui-menu-item:active, .jsui-menu-checked {border-style: inset;}
         .jsui-menu-disabled {padding: 4px 6px; border-width: 0px; filter: contrast(25%);}
         .jsui-basic-menu, .jsui-drop-menu {margin: 0px; padding: 0px; user-select: none;}
         .jsui-basic-menu {display: flex; gap: 1px;}
         .jsui-basic-menu > * {flex: 1;}
-        .jsui-table {border-width: 1px; border-classes: solid;}
+        .jsui-table {border-width: 1px; border-style: solid;}
         .jsui-table > * {display: flex; gap: 1px; margin: 1px;}
-        .jsui-table > * > * {flex: 1; padding: 3px 5px; text-align: center; line-height: 100%; border-width: 1px; border-classes: solid;}
+        .jsui-table > * > * {flex: 1; padding: 3px 5px; text-align: center; line-height: 100%; border-width: 1px; border-style: solid;}
         .jsui-table-title > * {background-color: #000000; color: #ffffff;}
         .jsui-notify-overlay {position: fixed; top: 20px; left: 0px; z-index: 99999999;}
         .jsui-notify-popup {position: relative; background-color: #fff; cursor: pointer; padding: 5px 10px; margin: 5px; width: fit-content; border-radius: 3px; border: 1px outset #cccccc;}`);
