0080774f76536991882af4c1b37b0136a712fd5a
jc3213 <jc3213@users.noreply.github.com>
2023-05-21 23:40:49 +0800

    Update sample.js

--- sample/sample.js
+++ sample/sample.js
@@ -17,7 +17,7 @@ textarea {width: 100%; resize: none;}
 .jsui-table {height: 400px;}
 .jsui-menu-item, .jsui-menu-cell {background-color: #23ade5; color: #fff;}`);
 
-jsUI.get('#filereader').onchange(async function (event) {
+jsUI.get('#filereader').onchange(async (event) => {
     var file = event.target.files[0];
     var text = await filereader.text(file);
     jsUI.get('#reader').value = text;
@@ -26,12 +26,13 @@ jsUI.get('#filereader').onchange(async function (event) {
 jsUI.get('#submit').onclick(function (event) {
     var url = jsUI.get('#url').value;
     url_result.empty();
-    url_components.get(url).then(getURLComponnents).catch(errorURLFormat);
+    var result = url_components.get(url);
+    getURLComponnents(result);
 });
 
 function getURLComponnents(components) {
-    Object.entries(components).forEach(function (entry) {
-        var [key, value] = entry;
+    Object.keys(components).forEach((key) => {
+        var value = components[key];
         var list = url_template.cloneNode(true);
         var label = list.querySelector('label');
         var input = list.querySelector('input');
@@ -50,24 +51,18 @@ function errorURLFormat(error) {
 var manager = jsUI.new().class('jsui-manager').parent(document.body);
 
 var menu = jsUI.menu().parent(manager);
-menu.add('按钮1').attr({id: 'btn1', title: '中文'}).onclick(clickBtnA);
-menu.add('button2').attr({id: 'btn2', title: 'English'}).onclick(clickBtnB);
-menu.add('ボタン3').attr({id: 'btn3', title: '日本語'}).onclick(clickBtnC);
-
-function clickBtnA(event) {
+menu.add('按钮1').attr({id: 'btn1', title: '中文'}).onclick((event) => {
     jsUI.notification('按钮1', 3000);
     entry.value = '按钮1';
-}
-
-function clickBtnB(event) {
+});
+menu.add('button2').attr({id: 'btn2', title: 'English'}).onclick((event) => {
     jsUI.notification('Button2', 3000);
     entry.value = 'Button2';
-}
-
-function clickBtnC(event) {
+});
+menu.add('ボタン3').attr({id: 'btn3', title: '日本語'}).onclick((event) => {
     jsUI.notification('ボタン3', 3000).onclick(event => alert('ボタン3を押しました！'));
     entry.value = 'ボタン3';
-}
+});
 
 var entry = jsUI.new('textarea').attr('rows', 6).parent(manager);
 
@@ -77,7 +72,7 @@ jsUI.dragndrop(entry, [dropzone, manager]);
 dropzone.addEventListener('drop', event => {
     entry.value = 'Drag\'n\'Drop Complete\n已成功完成拖拽\nドラッグアンドドロップ成功';
 });
-manager.addEventListener('drop', function (event) {
+manager.addEventListener('drop', (event) => {
     menu.after(entry);
     entry.value = 'Restored Position\n回到原来的位置\n元の位置に戻りました';
 });
