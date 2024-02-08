4017f2570bcc1dd30b6a98d9438c81e76418bf5b
jc3213 <jc3213@users.noreply.github.com>
2024-02-08 10:11:03 +0800

    Create aria2.xs.js

---
+++ aria2/aria2.xs.js
@@ -0,0 +1,13 @@
+class Aria2 {
+    constructor (url, secret) {
+        this.jsonrpc = url;
+        this.secret = `token:${secret}`;
+    }
+    fetch (...messages) {
+        const json = messages.map(({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this.secret, ...params] }));
+        return fetch(this.jsonrpc, {method: 'POST', body}).then((response) => {
+            if (response.ok) { return response.json(); }
+            throw new Error(response.statusText);
+        }).then((json) => json.map(({result, error}) => { if (result) { return result; } throw error }));
+    }
+}
