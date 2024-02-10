573995d2bdc8bd74c50559d8e5965f05e43b1ea2
jc3213 <jc3213@users.noreply.github.com>
2024-02-10 23:24:01 +0800

    Update aria2.js

--- aria2/aria2.js
+++ aria2/aria2.js
@@ -1,19 +1,28 @@
 class Aria2 {
     constructor (scheme, host, secret) {
-        this.host = host;
-        this.secret = `token:${secret}`;
-        this.method = scheme;
-        this.websocket = this.connect(this.jsonrpc.replace('http', 'ws'));
+        this._host = host;
+        this.scheme = scheme;
+        this.secret = secret;
+        this.connect();
     }
-    set method (scheme) {
+    set scheme (scheme) {
         const methods = { 'http': this.fetch, 'https': this.fetch, 'ws': this.send, 'wss': this.send };
-        this.jsonrpc = `${scheme}://${this.host}/jsonrpc`;
+        this._scheme = scheme;
+        this._jsonrpc = `${scheme}://${this._host}/jsonrpc`;
         this.post = methods[scheme];
         if (!this.post) { throw new Error(`Invalid protocol: "${scheme}" is not supported.`); }
     }
-    connect (url) {
-        return new Promise((resolve, reject) => {
-            const websocket = new WebSocket(url);
+    set host (host) {
+        this._host = host;
+        this._jsonrpc = `${this._scheme}://${host}/jsonrpc`;
+        this.disconnect().then( () => this.connect() );
+    }
+    set secret (secret) {
+        this._secret = `token:${secret}`;
+    }
+    connect () {
+        this.websocket = new Promise((resolve, reject) => {
+            const websocket = new WebSocket(this._jsonrpc.replace('http', 'ws'));
             websocket.onopen = (event) => resolve(websocket);
             websocket.onerror = (error) => reject(error);
         });
@@ -34,13 +43,13 @@ class Aria2 {
         });
     }
     fetch (body) {
-        return fetch(this.jsonrpc, {method: 'POST', body}).then((response) => {
+        return fetch(this._jsonrpc, {method: 'POST', body}).then((response) => {
             if (response.ok) { return response.json(); }
             throw new Error(response.statusText);
         });
     }
     call (...messages) {
-        const json = messages.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this.secret, ...params]}) );
+        const json = messages.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this._secret, ...params]}) );
         return this.post(JSON.stringify(json)).then( (response) => response.map(({result, error}) => { if (result) { return result; } throw error; }) );
     }
 }
