a8f84decf798831996fb735ac02d9373c532f218
jc3213 <jc3213@users.noreply.github.com>
2024-02-11 14:34:23 +0800

    Update aria2.js

--- aria2/aria2.js
+++ aria2/aria2.js
@@ -1,42 +1,28 @@
 class Aria2 {
     constructor (scheme, host, secret) {
-        this._host = host;
-        this.scheme = scheme;
-        this.secret = secret;
-        this.websocket = this.connect();
+        this.host = host;
+        this.secret = `token:${secret}`;
+        this.method = scheme;
+        this.websocket = this.connect(this.jsonrpc.replace('http', 'ws'));
     }
-    set scheme (scheme) {
+    set method (scheme) {
         const methods = { 'http': this.fetch, 'https': this.fetch, 'ws': this.send, 'wss': this.send };
-        this._scheme = scheme;
-        this._jsonrpc = `${scheme}://${this._host}/jsonrpc`;
+        this.jsonrpc = `${scheme}://${this.host}/jsonrpc`;
         this.post = methods[scheme];
         if (!this.post) { throw new Error(`Invalid protocol: "${scheme}" is not supported.`); }
     }
-    set host (host) {
-        this._host = host;
-        this._jsonrpc = `${this._scheme}://${host}/jsonrpc`;
-        this.disconnect().then(() => {
-            this.websocket = this.connect();
-            this.websocket.then( (websocket) => this.onmessage = this.messager );
-        });
-    }
-    set secret (secret) {
-        this._secret = `token:${secret}`;
-    }
-    connect () {
+    connect (url) {
         return new Promise((resolve, reject) => {
-            const websocket = new WebSocket(this._jsonrpc.replace('http', 'ws'));
+            const websocket = new WebSocket(url);
             websocket.onopen = (event) => resolve(websocket);
             websocket.onerror = (error) => reject(error);
         });
     }
     disconnect () {
-        return this.websocket.then( (websocket) => websocket.close() );
+        this.websocket.then( (websocket) => websocket.close() );
     }
     set onmessage (callback) {
-        if (typeof callback !== 'function') { return; }
-        this.messager = callback;
-        this.websocket.then( (websocket) => websocket.addEventListener('message', (event) => this.messager(JSON.parse(event.data))) );
+        this.websocket.then( (websocket) => websocket.addEventListener('message', (event) => callback(JSON.parse(event.data))) );
     }
     send (message) {
         return new Promise((resolve, reject) => {
@@ -48,13 +34,13 @@ class Aria2 {
         });
     }
     fetch (body) {
-        return fetch(this._jsonrpc, {method: 'POST', body}).then((response) => {
+        return fetch(this.jsonrpc, {method: 'POST', body}).then((response) => {
             if (response.ok) { return response.json(); }
             throw new Error(response.statusText);
         });
     }
     call (...messages) {
-        const json = messages.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this._secret, ...params] }) );
+        const json = messages.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this.secret, ...params] }) );
         return this.post(JSON.stringify(json)).then( (response) => response.map(({result, error}) => { if (result) { return result; } throw error; }) );
     }
 }
