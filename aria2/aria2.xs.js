60ea7ca0b3a7636f989a7087d300469012f38a0c
jc3213 <jc3213@users.noreply.github.com>
2024-02-12 09:53:39 +0800

    Update aria2.xs.js

--- aria2/aria2.xs.js
+++ aria2/aria2.xs.js
@@ -6,8 +6,7 @@ class Aria2 {
     post (...messages) {
         const body = JSON.stringify(messages.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this.secret, ...params]}) ));
         return fetch(this.jsonrpc, {method: 'POST', body}).then((response) => {
-            if (response.ok) { return response.json(); }
-            throw new Error(response.statusText);
+            if (response.ok) { return response.json(); } throw new Error(response.statusText);
         }).then( (json) => json.map( ({result, error}) => { if (result) { return result; } throw error; } ) );
     }
 }
