35beb06caeb32ffa2da27364bc72069aa0b196ae
jc3213 <jc3213@users.noreply.github.com>
2024-02-12 09:45:21 +0800

    Update aria2.js

--- aria2/aria2.js
+++ aria2/aria2.js
@@ -8,8 +8,8 @@ class Aria2 {
     set method (scheme) {
         const methods = { 'http': this.fetch, 'https': this.fetch, 'ws': this.send, 'wss': this.send };
         this.jsonrpc = scheme + '://' + this.host + '/jsonrpc';
-        this.post = methods[scheme];
-        if (!this.post) { throw new Error('Invalid method: ' + scheme + ' is not supported!'); }
+        this.call = methods[scheme];
+        if (!this.call) { throw new Error('Invalid method: ' + scheme + ' is not supported!'); }
     }
     connect () {
         this.websocket = new Promise((resolve, reject) => {
@@ -24,23 +24,24 @@ class Aria2 {
     set onmessage (callback) {
         this.websocket.then( (websocket) => websocket.addEventListener('message', (event) => callback(JSON.parse(event.data))) );
     }
-    send (message) {
-        return new Promise((resolve, reject) => {
+    messager (entry, callback) {
+        const json = entry.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this.secret, ...params] }) );
+        return callback(JSON.stringify(json)).then((response) => {
+            return response.map( ({result, error}) => { if (result) { return result; } throw error; } );
+        });
+    }
+    send (...entry) {
+        return this.messager(entry, (message) => new Promise((resolve, reject) => {
             this.websocket.then((websocket) => {
                 websocket.onmessage = (event) => resolve(JSON.parse(event.data));
                 websocket.onerror = (error) => reject(error);
                 websocket.send(message);
             });
-        });
+        }));
     }
-    fetch (body) {
-        return fetch(this.jsonrpc, {method: 'POST', body}).then((response) => {
-            if (response.ok) { return response.json(); }
-            throw new Error(response.statusText);
-        });
-    }
-    call (...messages) {
-        const json = messages.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this.secret, ...params] }) );
-        return this.post(JSON.stringify(json)).then( (response) => response.map(({result, error}) => { if (result) { return result; } throw error; }) );
+    fetch (...entry) {
+        return this.messager(entry, (body) => fetch(this.jsonrpc, {method: 'POST', body}).then((response) => {
+            if (response.ok) { return response.json(); } throw new Error(response.statusText);
+        }));
     }
 }
