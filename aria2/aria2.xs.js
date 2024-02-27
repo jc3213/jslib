5ea13dea9eb8e4fed16c166b983c956262fbc661
jc3213 <jc3213@users.noreply.github.com>
2024-02-27 10:21:21 +0800

    Update aria2.xs.js

--- aria2/aria2.xs.js
+++ aria2/aria2.xs.js
@@ -1,12 +1,22 @@
-class Aria2 {
+class Aria2XMLRequest {
     constructor (url, secret) {
         this.jsonrpc = url;
         this.secret = `token:${secret}`;
     }
-    post (...messages) {
-        const body = JSON.stringify(messages.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this.secret, ...params] }) ));
-        return fetch(this.jsonrpc, {method: 'POST', body}).then((response) => {
+    get (...messages) {
+        return fetch(this.jsonrpc + '?' + btoa(this.json(messages))).then((response) => {
             if (response.ok) { return response.json(); } throw new Error(response.statusText);
-        }).then( (json) => json.map( ({result, error}) => { if (result) { return result; } throw error; } ) );
+        }).then(this.proc);
+    }
+    post (...messages) {
+        return fetch(this.jsonrpc, {method: 'POST', body: this.json(messages)}).then((response) => {
+            if (response.ok) { return response.json(); } throw new Error(response.statusText);
+        }).then(this.proc);
+    }
+    json (array) {
+        return JSON.stringify(array.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this.secret, ...params] }) ));
+    }
+    proc (array) {
+        return array.map( ({result, error}) => { if (result) { return result; } throw error; } );
     }
 }
