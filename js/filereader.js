5f6c9d52a3a589b15411209b7fd98f6cd2000a5a
jc3213 <jc3213@users.noreply.github.com>
2024-03-20 10:44:38 +0800

    Update filereader.js

--- js/filereader.js
+++ js/filereader.js
@@ -2,7 +2,7 @@ class PromiseFileReader {
     reader (file, method) {
         return new Promise((resolve, reject) => {
             var reader = new FileReader();
-            reader.onload = (event) => resolve(reader.result);
+            reader.onload = (event) => resolve(event.target.result);
             reader.onerror = reject;
             reader[method](file);
         });
