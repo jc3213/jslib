125a8055e0bfa64b5365842dba45baccca564d4a
jc3213 <jc3213@users.noreply.github.com>
2024-05-03 22:50:31 +0800

    Add aria2_0.7.0.js

---
+++ aria2/archived/aria2_0.7.0.js
@@ -0,0 +1,89 @@
+class Aria2 {
+    constructor (...args) {
+        let path = args.join('#').match(/^(https?|wss?)(?:#|:\/\/)([^#]+)#?(.*)$/);
+        if (!path) { throw new Error('Invalid JSON-RPC entry: "' + args.join('", "') + '"'); }
+        this.jsonrpc = {};
+        this.scheme = path[1];
+        this.url = path[2];
+        this.secret = path[3];
+    }
+    set scheme (scheme) {
+        this.call = { 'http': this.post, 'https': this.post, 'ws': this.send, 'wss': this.send }[ scheme ];
+        if (!this.call) { throw new Error('Invalid JSON-RPC scheme: "' + scheme + '" is not supported!'); }
+        this.jsonrpc.scheme = scheme;
+        this.jsonrpc.path = scheme + '://' + this.jsonrpc.url;
+    }
+    get scheme () {
+        return this.jsonrpc.scheme;
+    }
+    set url (url) {
+        if (this.jsonrpc.url === url) { return; }
+        this.jsonrpc.url = url;
+        this.jsonrpc.path = this.jsonrpc.scheme + '://' + url;
+        this.jsonrpc.ws = this.jsonrpc.path.replace('http', 'ws');
+        this.connect();
+    }
+    get url () {
+        return this.jsonrpc.url;
+    }
+    set secret (secret) {
+        this.jsonrpc.secret = secret;
+        this.jsonrpc.params = secret ? ['token:' + secret] : [];
+    }
+    get secret () {
+        return this.jsonrpc.secret;
+    }
+    connect () {
+        this.socket?.then( (ws) => ws.close() );
+        this.socket = new Promise((resolve, reject) => {
+            let ws = new WebSocket(this.jsonrpc.ws);
+            ws.onopen = (event) => resolve(ws);
+            ws.onerror = reject;
+        });
+        this.listener('message', this.jsonrpc.onmessage);
+        this.listener('close', this.jsonrpc.onclose);
+    }
+    listener (type, callback) {
+        let listener = 'on' + type;
+        if (typeof callback === 'function') {
+            this.jsonrpc[listener] = callback;
+            if (!this.jsonrpc[type]) {
+                this.socket.then( (ws) => ws.addEventListener(type, callback) );
+                this.jsonrpc[type] = true;
+            }
+        } else if (this.jsonrpc[type]) {
+            this.socket.then( (ws) => ws.removeEventListener(type, this.jsonrpc[listener]) );
+            this.jsonrpc[type] = false;
+            this.jsonrpc[listener] = null;
+        }
+    }
+    set onmessage (callback) {
+        this.listener('message', callback);
+    }
+    get onmessage () {
+        return this.jsonrpc.onmessage;
+    }
+    set onclose (callback) {
+        this.listener('close', callback);
+    }
+    get onclose () {
+        return this.jsonrpc.onclose;
+    }
+    send (...messages) {
+        return this.socket.then((ws) => new Promise((resolve, reject) => {
+            ws.onmessage = (event) => resolve(JSON.parse(event.data));
+            ws.onerror = reject;
+            ws.send(this.json(messages));
+        }));
+    }
+    post (...messages) {
+        return fetch(this.jsonrpc.path, {method: 'POST', body: this.json(messages)}).then((response) => {
+            if (response.ok) { return response.json(); }
+            throw new Error(response.statusText);
+        });
+    }
+    json (array) {
+        let json = array.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [...this.jsonrpc.params, ...params] }) );
+        return JSON.stringify(json);
+    }
+}
