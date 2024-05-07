04523af080a83fcb7d0a524abb2d870f7fe865cb
jc3213 <jc3213@users.noreply.github.com>
2024-05-07 14:16:22 +0800

    Update aria2_0.7.0.js

--- aria2/archived/aria2_0.7.0.js
+++ aria2/archived/aria2_0.7.0.js
@@ -45,7 +45,6 @@ class Aria2 {
             ws.onerror = reject;
             ws.onclose = (event) => {
                 if (ws.abort) { return ws = null; }
-                this.jsonrpc.close = this.jsonrpc.message = false;
                 setTimeout(this.connect.bind(this), 5000);
             };
         });
@@ -55,14 +54,20 @@ class Aria2 {
     listener (type, callback) {
         let listener = 'on' + type;
         if (typeof callback === 'function') {
+            this.socket.then((ws) => {
+                if (!ws[type]) {
+                    ws.addEventListener(type, callback);
+                    ws[type] = true;
+                }
+            });
             this.jsonrpc[listener] = callback;
-            if (!this.jsonrpc[type]) {
-                this.socket.then( (ws) => ws.addEventListener(type, callback) );
-                this.jsonrpc[type] = true;
-            }
-        } else if (this.jsonrpc[type]) {
-            this.socket.then( (ws) => ws.removeEventListener(type, this.jsonrpc[listener]) );
-            this.jsonrpc[type] = false;
+        } else {
+            this.socket.then((ws) => {
+                if (ws[type]) {
+                    ws.removeEventListener(type, this.jsonrpc[listener]);
+                    ws[type] = false;
+                }
+            });
             this.jsonrpc[listener] = null;
         }
     }
