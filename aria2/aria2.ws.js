ef1ed48be2d4b51cc7b509f5d87f43b8eae0a179
jc3213 <jc3213@users.noreply.github.com>
2024-05-07 15:20:02 +0800

    Update aria2.ws.js

--- aria2/aria2.ws.js
+++ aria2/aria2.ws.js
@@ -1,11 +1,15 @@
 class Aria2WebSocket {
     constructor (url, secret) {
         this.jsonrpc = url;
-        this.secret = `token:${secret}`;
-        this.websocket =  new Promise((resolve, reject) => {
+        this.secret = secret;
+        this.params = secret ? ['token:' + secret] : [];
+        this.connect();
+    }
+    connect () {
+        this.websocket = new Promise((resolve, reject) => {
             const websocket = new WebSocket(this.jsonrpc);
             websocket.onopen = (event) => resolve(websocket);
-            websocket.onerror = (error) => reject(error);
+            websocket.onclose = (error) => setTimeout(this.connect.bind(this), 5000);
         });
     }
     set onclose (callback) {
@@ -25,7 +29,7 @@ class Aria2WebSocket {
         return this._onmessage;
     }
     send (...messages) {
-        const message = JSON.stringify(messages.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [this.secret, ...params] }) ));
+        const message = JSON.stringify(messages.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [...this.params, ...params] }) ));
         return new Promise((resolve, reject) => {
             this.websocket.then((websocket) => {
                 websocket.onmessage = (event) => resolve(JSON.parse(event.data));
