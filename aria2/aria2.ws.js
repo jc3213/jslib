a74bf337c3a64fe0b9f58ff17338570689a566da
jc3213 <jc3213@users.noreply.github.com>
2024-05-08 08:33:35 +0800

    Update aria2.ws.js

--- aria2/aria2.ws.js
+++ aria2/aria2.ws.js
@@ -8,7 +8,7 @@ class Aria2WebSocket {
     connect () {
         this.socket?.then( (ws) => ws.close() );
         this.socket = new Promise((resolve, reject) => {
-            let ws = new WebSocket(this.jsonrpc.ws);
+            let ws = new WebSocket(this.jsonrpc);
             ws.onopen = (event) => resolve(ws);
             ws.onmessage = (event) => {
                 let response = JSON.parse(event.data);
@@ -36,7 +36,7 @@ class Aria2WebSocket {
         return this.socket.then((ws) => new Promise((resolve, reject) => {
             ws.resolve = resolve;
             ws.onerror = reject;
-            ws.send( JSON.stringify( array.map( ({method, params = []}) => ({id: '', jsonrpc: '2.0', method, params: [...this.jsonrpc.params, ...params]}) ) ) );
+            ws.send( JSON.stringify( array.map( ({method, params = []}) => ({id: '', jsonrpc: '2.0', method, params: [...this.params, ...params]}) ) ) );
         }));
     }
 }
