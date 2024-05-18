b0d4ea9637347782dbf938604e09c2ca21af4194
jc3213 <jc3213@users.noreply.github.com>
2024-05-18 14:48:12 +0800

    Update aria2.xs.js

--- aria2/aria2.xs.js
+++ aria2/aria2.xs.js
@@ -5,14 +5,13 @@ class Aria2XMLRequest {
         this.params = secret ? ['token:' + secret] : [];
     }
     get (...messages) {
-        return fetch(this.jsonrpc + '?params=' + btoa(unescape(encodeURIComponent(this.json(messages))))).then((response) => {
-            if (response.ok) { return response.json(); } throw new Error(response.statusText);
-        });
+        return fetch(this.jsonrpc + '?params=' + btoa(unescape(encodeURIComponent(this.json(messages))))).then(this.result);
     }
     post (...messages) {
-        return fetch(this.jsonrpc, {method: 'POST', body: this.json(messages)}).then((response) => {
-            if (response.ok) { return response.json(); } throw new Error(response.statusText);
-        });
+        return fetch(this.jsonrpc, {method: 'POST', body: this.json(messages)}).then(this.result);
+    }
+    result (response) {
+        if (response.ok) { return response.json(); } throw new Error(response.statusText);
     }
     json (array) {
         return JSON.stringify(array.map( ({method, params = []}) => ({ id: '', jsonrpc: '2.0', method, params: [...this.params, ...params] }) ));
