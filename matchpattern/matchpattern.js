f4d1699be5c86e53d8dde5db324c11cbc3e427cc
jc3213 <jc3213@users.noreply.github.com>
2024-05-31 00:38:49 +0800

    Update matchpattern.js

--- matchpattern/matchpattern.js
+++ matchpattern/matchpattern.js
@@ -1,104 +1,28 @@
 class MatchPattern {
-    constructor (hosts) {
-        hosts?.forEach((host) => this.add(host));
+    constructor (args) {
+        this.rule = Array.isArray(args) ? args : [];
+        this.cache = {};
+        this.make();
     }
-    add (url) {
-        const result = this.make(url);
-        this.hosts.push(host);
-        this.matchpatterns.push(result);
-        this.generator();
-        return result;
+    add (rule) {
+        if (this.cache[rule]) { return; }
+        this.cache[rule] = true;
+        this.rule.push(rule);
+        this.make();
     }
-    remove (url) {
-        const host = new URL(url).hostname;
-        const index = this.hosts.indexOf(host);
-        if (index === -1) {
-            return;
-        }
-        this.hosts.splice(index, 1);
-        this.matchpatterns.splice(index, 1);
-        this.generator();
+    remove (rule) {
+        if (!this.cache[rule]) { return; }
+        this.rule.splice(this.rule.indexOf(rule), 1);
+        this.cache[rule] = false;
+        this.make();
     }
-    make (url) {
-        const host = new URL(url).hostname;
-        if (this.caches[host]) {
-            return this.caches[host];
-        }
-        if (/((25[0-5]|(2[0-4]|1[0-9]|[1-9]?)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])/.test(host)) {
-            return this.ipv4(host);
-        }
-        let result;
-        let [tld, sld, sub, ...useless] = host.split('.').reverse();
-        if (sld === undefined) {
-            result = hostname;
-        }
-        else if (sld in this.tlds) {
-            result = '*.' + sub + '.' + sld + '.' + tld;
-        }
-        else {
-            result = '*.' + sld + '.' + tld;
-        }
-        this.caches[host] = result;
-        return result;
-    }
-    ipv4 (ipv4) {
-        let [network, host, ...useless] = ipv4.split('.');
-        let result = network + '.' + host + '.*';
-        this.caches[ipv4] = result;
-        return result;
-    }
-    generator () {
-        if (this.matchpatterns.length === 0) {
-            this.regexp = /!/;
-        }
-        else {
-            const patterns = [...new Set(this.matchpatterns)];
-            this.regexp = new RegExp('^(' + patterns.join('|').replace(/\./g, '\\.').replace(/\\?\.?\*\\?\.?/g, '.*') + ')$');
-        }
+    make () {
+        this.regexp = this.rule.length === 0 ? /!/ : new RegExp('^(' + this.rule.join('|').replace(/\./g, '\\.').replace(/\\?\.?\*\\?\.?/g, '.*') + ')$');
+        this.result = {};
     }
     match (url) {
-        const host = new URL(url).hostname;
-        return this.regexp.test(host);
+        let host = new URL(url).hostname;
+        if (host in this.result) { return this.result[host]; }
+        return this.result[host] = this.regexp.test(host);
     }
-    caches = {};
-    hosts = [];
-    matchpatterns = [];
-    regexp = /!/;
-    tlds = {
-        'aero': true,
-        'app': true,
-        'arpa': true,
-        'asia': true,
-        'biz': true,
-        'cat': true,
-        'co': true,
-        'com': true,
-        'coop': true,
-        'dev': true,
-        'edu': true,
-        'eu': true,
-        'gov': true,
-        'info': true,
-        'int': true,
-        'io': true,
-        'jobs': true,
-        'ltd': true,
-        'ltda': true,
-        'mil': true,
-        'mobi': true,
-        'museum': true,
-        'name': true,
-        'ne': true,
-        'net': true,
-        'org': true,
-        'post': true,
-        'pro': true,
-        'si': true,
-        'tel': true,
-        'test': true,
-        'travel': true,
-        'web': true,
-        'xxx': true,
-        'xyz': true
-    };
 }
