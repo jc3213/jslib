e6e7b04ea83346c98776d79ebdbcc2c65c109ddc
jc3213 <jc3213@users.noreply.github.com>
2024-07-12 19:58:22 +0800

    Update gachaSimulator.js

--- gachaSimulator.js
+++ gachaSimulator.js
@@ -2,22 +2,40 @@ class Gacha {
     constructor(posibility, threshold) {
         this.posibility = posibility;
         this.threshold = threshold;
+        this.reset();
+    }
+    reset () {
         this.count = 0;
+        this.result = { r: 0, sr: 0, ssr: 0, pickup: 0 };
         this.pickup = [];
     }
     roll (number) {
         let time = number | 0;
         if (time < 1) { time = 1; }
-        for (let i = 0; i < time; i ++) {
-            this.count ++;
-            if (Math.random() < this.posibility) {
+        for (let i = 0; i < time; i++) {
+            this.count++;
+            let result = Math.random();
+            if (result < this.posibility) {
+                this.result.pickup ++;
                 this.pickup.push(this.count);
-                console.log('Gacha! at ' + this.count);
+                console.log('Got pickup card at ' + this.count);
+            } else {
+                let ssr = 0.05;
+                let sr = 0.30;
+                if (result > sr) {
+                    this.result.r ++;
+                    console.log('Got an R card!');
+                } else if (result > ssr) {
+                    this.result.sr ++;
+                    console.log('Got an SR card!');
+                } else {
+                    this.result.ssr ++;
+                    console.log('Got an SSR card!');
+                }
             }
-            else if (this.count === this.threshold) {
-                this.count = 0;
-                this.pickup = [];
-                console.log('Pickup! at ' + this.threshold);
+            if (this.count === this.threshold) {
+                console.log('Gacha threshold at ' + this.threshold + 'rolls;\nGot ' + this.result.pickup + ' pickup cards at "' + this.pickup.join(', ') + '";\nGot ' + this.result.r + ' R cards, ' + this.result.sr + ' SR cards, ' + this.result.ssr + ' SSR cards;');
+                this.reset();
                 break;
             }
         }
