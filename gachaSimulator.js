0f4e9f053f957e7b587c11657f78174b790af850
jc3213 <jc3213@users.noreply.github.com>
2024-07-12 23:22:15 +0800

    Update gachaSimulator.js

--- gachaSimulator.js
+++ gachaSimulator.js
@@ -1,16 +1,21 @@
 class Gacha {
     constructor() {
-        this.rarity = { sr: 0.3, ssr: 0.05, pickup: 0.0007 };
-        this.pools = { r: this.pool(50), sr: this.pool(15), ssr: this.pool(5) };
+        this.rarity = { sr: 0.3, ssr: 0.05 };
+        this.pools = { r: this.pool('r', 80), sr: this.pool('sr', 30), ssr: this.pool('ssr', 10) };
         this.threshold = 200;
+        this.pickup(0.007);
         this.reset();
     }
     reset () {
         this.total = 0;
-        this.result = { r: 0, sr: 0, ssr: 0, up: 0, pickup: [], logs: [] };
+        this.result = { r: 0, sr: 0, ssr: 0, up: 0, bingo: [], logs: [] };
     }
-    pool (length) {
-        return Array.from({ length }, (_, i) => 'r' + (i + 1));
+    pickup (min) {
+        this.rarity.up = this.rarity.ssr / this.pools.ssr.length;
+        if (this.rarity.up < min) { this.rarity.up = min; }
+    }
+    pool (type, length) {
+        return Array.from({ length }, (_, i) => type + (i + 1));
     }
     card (type) {
         let pool = this.pools[type];
@@ -26,10 +31,10 @@ class Gacha {
             if (this.total === this.threshold) { break; }
             this.total ++;
             let random = Math.random();
-            if (random < this.rarity.pickup) {
+            if (random < this.rarity.up) {
                 this.result.up ++;
                 this.result.logs.push('pickup');
-                this.result.pickup.push(this.total);
+                this.result.bingo.push(this.total);
                 console.log('Got pickup card, roll counts ' + this.total);
             } else if (random < this.rarity.ssr) {
                 this.card('ssr');
@@ -39,7 +44,7 @@ class Gacha {
                 this.card('r');
             }
             if (this.total === this.threshold) {
-                console.log('Gacha threshold at ' + this.threshold + ' rolls;\nGot ' + this.result.up + ' pickup cards at "' + this.result.pickup.join(', ') + '";\nGot ' + this.result.r + ' R cards, ' + this.result.sr + ' SR cards, ' + this.result.ssr + ' SSR cards;');
+                console.log('Gacha threshold at ' + this.threshold + ' rolls;\nGot ' + this.result.up + ' pickup cards at "' + this.result.bingo.join(', ') + '";\nGot ' + this.result.r + ' R cards, ' + this.result.sr + ' SR cards, ' + this.result.ssr + ' SSR cards;');
             }
         }
     }
